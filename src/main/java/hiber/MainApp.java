873714b6816e407bb7c20a6f3894d97acddeb4da
Kraskovskii <kraskovskii.m@yandex.ru>
2021-07-12 14:37:03 +0300

    v4

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -6,7 +6,6 @@ import hiber.model.User;
 import hiber.service.UserService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import java.sql.SQLException;
 import java.util.List;
 
 public class MainApp {
@@ -16,14 +15,20 @@ public class MainApp {
 
         UserService userService = context.getBean(UserService.class);
 
-        userService.add(new User("User1", "Lastname1", "user1@mail.ru",
-                new Car("VAZ", 1)));
-        userService.add(new User("User2", "Lastname2", "user2@mail.ru",
-                new Car("Opel", 12)));
-        userService.add(new User("User3", "Lastname3", "user3@mail.ru",
-                new Car("Reno", 123)));
-        userService.add(new User("User4", "Lastname4", "user4@mail.ru",
-                new Car("Mercedes", 1234)));
+        User user1 = new User("User1", "Lastname1", "user1@mail.ru");
+        User user2 = new User("User2", "Lastname2", "user2@mail.ru");
+        User user3 = new User("User3", "Lastname3", "user3@mail.ru");
+        User user4 = new User("User4", "Lastname4", "user4@mail.ru");
+
+        user1.setCar(new Car("VAZ", 1));
+        user2.setCar(new Car("Mercedes", 12));
+        user3.setCar(new Car("Reno", 123));
+        user4.setCar(new Car("Opel", 12));
+
+        userService.add(user1);
+        userService.add(user2);
+        userService.add(user3);
+        userService.add(user4);
 
         List<User> users = userService.listUsers();
 
@@ -38,7 +43,7 @@ public class MainApp {
 
 //        5. В сервис добавьте метод, который с помощью hql-запроса будет доставать
 //        юзера, владеющего машиной по ее модели и серии.
-      System.out.println(userService.getUser(new Car("VAZ", 1)));
+        System.out.println(userService.getUser(new Car("VAZ", 1)));
 
         context.close();
     }
